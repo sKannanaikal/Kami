a8e45e948c7726d68ce795f1513120cf8990bae9
Sean Kannanaikal <sean_kannanaikal@hotmail.com>
2026-04-04 08:55:43 -0400

    added capabilities to pull functionality from kami binary library

--- test/kami_test.cpp
+++ test/kami_test.cpp
@@ -1,9 +1,18 @@
 #include <gtest/gtest.h>
+#include "../src/lib/test.h"
 
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
+TEST(KamiTest, BasicAssertions) {
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
   // Expect equality.
   EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(KamiTest, BasicArithmetic) {
+  EXPECT_EQ(add(3, 2), 5);
+
+  EXPECT_EQ(subtract(3, 2), 1);
+  
+  EXPECT_EQ(multiply(3, 2), 6);
 }
