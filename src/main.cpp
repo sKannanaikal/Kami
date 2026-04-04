9982fa4e9f9c3987d6bdfaeffe844639a856c5cc
Sean Kannanaikal <sean_kannanaikal@hotmail.com>
2026-04-04 07:23:36 -0400

    basic pr adding multiplication

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ int main() {
 	std::cout << "Hello World!" << std::endl;
 	std::cout << add(3, 2) << std::endl;
 	std::cout << subtract(3, 2) << std::endl;
+	std::cout << multiply(3, 2) << std::endl;
 	std::cout << "Good Bye World!" << std::endl;
 	return 0;
 }
